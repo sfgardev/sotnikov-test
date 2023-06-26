96c2d35cc500fa714e79c9b711205b002d4a7098
sfgardev <sf.garol@gmail.com>
2023-06-26 12:56:10 +0800

    changed styles for tab panel

--- src/components/Tabs/TabPanel.tsx
+++ src/components/Tabs/TabPanel.tsx
@@ -6,22 +6,21 @@ interface TabPanelProps {
   value: number;
 }
 
+const tabsSize = 49;
+
 export function TabPanel(props: TabPanelProps) {
   const { children, value, index, ...other } = props;
 
   return (
-    <div
+    <Box
+      sx={{ minHeight: `calc(100vh - ${tabsSize}px)` }}
       role="tabpanel"
       hidden={value !== index}
       id={`simple-tabpanel-${index}`}
       aria-labelledby={`simple-tab-${index}`}
       {...other}
     >
-      {value === index && (
-        <Box sx={{ p: 3 }}>
-          <Typography>{children}</Typography>
-        </Box>
-      )}
-    </div>
+      {value === index && <Box sx={{ p: 3, height: "100%", display: 'flex', alignItems:'center', justifyContent:'center' }}>{children}</Box>}
+    </Box>
   );
 }
