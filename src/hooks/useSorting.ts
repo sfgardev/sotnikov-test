488d6eb046e75bdc4ff2859c795759c0b76be3e1
sfgardev <sf.garol@gmail.com>
2023-06-29 17:46:19 +0800

    made flexible for different kind of fields

--- src/hooks/useSorting.ts
+++ src/hooks/useSorting.ts
@@ -1,14 +1,18 @@
 import { useState } from "react";
-import { SortBy, SortDirection } from "../components/Sorting";
+import { SortDirection } from "../components/Sorting";
 
-export default function useSorting() {
-  const [sortBy, setSortBy] = useState<SortBy>("");
-  const [sortDirection, setSortDirection] = useState<SortDirection>("");
+export default function useSorting<TSortBy>(
+  defaultSortBy = "" as TSortBy,
+  defaultSortDirection = "asc" as SortDirection
+) {
+  const [sortBy, setSortBy] = useState<TSortBy>(defaultSortBy as TSortBy);
+  const [sortDirection, setSortDirection] =
+    useState<SortDirection>(defaultSortDirection);
 
   const handleChangeSortDirection = (value: SortDirection) =>
     setSortDirection(value);
 
-  const handleChangeSortBy = (value: SortBy) => setSortBy(value);
+  const handleChangeSortBy = (value: TSortBy) => setSortBy(value);
 
   return {
     sortBy,
